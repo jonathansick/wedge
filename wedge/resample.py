7ac876f7b88232460e3e9aa80ba9d25d60e23f8e
Jonathan Sick <jonathansick@mac.com>
2014-02-07 10:49:03 -0500

    Create TargetWCS class for resampling
    The TargetWCS class computes the FITS WCS values appropriate for the
    resampled images so that the wedge axis is along the x-axis.

--- wedge/resample.py
+++ wedge/resample.py
@@ -8,6 +8,8 @@ This resampling code builds upon that in the Skyoffset and Mo'Astro packages,
 and in turn, uses Astromatic's Swarp to carry our the heavy pixel lifting.
 """
 
+import math
+
 from skyoffset.resample import MosaicResampler
 
 
@@ -64,3 +66,82 @@ def resample_images(image_paths, pa, radec_origin, pixel_scale, work_dir,
     # FIXME
     nx, ny = 1, 1
     swarp_configs['IMAGE SIZE'] = "{:d},{:d}".format(nx, ny)
+
+
+class TargetWCS(object):
+    """A synthesized world coordinate system to resampled images into
+    so that the wedge is oriented with the x-axis.
+
+    Parameters
+    ----------
+    radec_origin : tuple
+        A tuple of (RA, Dec), giving the location of the wedge origin (i.e.
+        the galaxy center).
+    pixel_scale : float
+        Pixel scale, arcseconds per pixel.
+    pa : float
+        Position angle of the wedge, in degrees.
+    wedge_box : tuple
+        Tuple of maximum wedge (length, width) as a tuple of floats. Units
+        are arcseconds.
+    """
+    def __init__(self, radec_origin, pixel_scale, pa, wedge_box):
+        super(TargetWCS, self).__init__()
+        self._radec_origin = radec_origin
+        self._pixel_scale = float(pixel_scale)
+        self._pa_deg = float(pa)
+        self._wedge_length = pixel_scale * wedge_box[0]
+        self._wedge_height = pixel_scale * wedge_box[1]
+        self._wcs_fields = self._compute_wcs()
+
+    def _compute_wcs(self):
+        """Compute the target world coordinate system.
+        
+        Returns
+        -------
+        wcs_fields : dict
+            A dictionary whose keys are FITS header fields, and values are
+            the computed WCS.
+        """
+        # total width, height, in pixels
+        NAXIS1 = self._ensure_odd(int(self._wedge_length * 2))
+        NAXIS2 = self._ensure_odd(int(self._wedge_height))
+        CRPIX1 = self._middle(NAXIS1)
+        CRPIX2 = self._middle(NAXIS2)
+        CRVAL1 = self._radec_origin[0]  # RA at center
+        CRVAL2 = self._radec_origin[1]  # Dec at center
+
+        pa = self._pa_deg * math.pi / 180.
+        s = self.pixel_scale / 3600.
+        CD1_1 = s * math.cos(pa)
+        CD2_1 = -s * math.sin(pa)
+        CD1_2 = s * math.sin(pa)
+        CD2_2 = s * math.cos(pa)
+
+        return {"NAXIS": 2,
+                "RADESYS": 'FK5',
+                "EQUINOX": '2000.0',
+                "CTYPE1": "RA---TAN",
+                "CTYPE2": "DEC--TAN",
+                "NAXIS1": NAXIS1,
+                "NAXIS2": NAXIS2,
+                "CRPIX1": CRPIX1,
+                "CRPIX2": CRPIX2,
+                "CRVAL1": CRVAL1,
+                "CRVAL2": CRVAL2,
+                "CD1_1": CD1_1,
+                "CD2_1": CD2_1,
+                "CD1_2": CD1_2,
+                "CD2_2": CD2_2}
+
+    def _ensure_odd(self, n):
+        """Ensure an integer number is odd."""
+        if not n % 2:
+            n += 1
+        return n
+
+    def _middle(self, n):
+        """Given an odd number of pixels, return the pixel in the middle.
+        1-based, as is the FITS standard.
+        """
+        return int(math.ceil(float(n) / 2.))
