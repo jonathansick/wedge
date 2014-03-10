d0b3f2856446b876a4cb31ebf3e2e08a5567b382
Jonathan Sick <jonathansick@mac.com>
2014-03-10 17:58:18 -0400

    Add set_unweighted_pixels_to_nan to resample
    This forces unweighted pixels (due to flag masking) to have a value of
    NaN, and thus be easily splotted when measuring photometry in the bins.

--- wedge/resample.py
+++ wedge/resample.py
@@ -98,6 +98,8 @@ def resample_images(image_paths, radec_origin, pixel_scale, pa,
     resamp_docs = resampler.resample("wedge", pix_scale=pixel_scale,
             swarp_configs=swarp_configs)
 
+    set_unweighted_pixels_to_nan(resamp_docs)
+
     wedge_image_paths = [doc['image_path'] for doc in resamp_docs]
     outputs = [wedge_image_paths]
     if weight_paths:
@@ -112,6 +114,21 @@ def resample_images(image_paths, radec_origin, pixel_scale, pa,
         outputs.append(resamp_background_sigmas)
 
     return outputs
+
+
+def set_unweighted_pixels_to_nan(docs):
+    """After processing by skyoffset's MosaicResample, this function sets
+    unweighted resampled pixels to NaN."""
+    for doc in docs:
+        if 'weight_path' in doc:
+            wpath = doc['weight_path']
+            impath = doc['image_path']
+            wfits = fits.open(wpath)
+            imfits = fits.open(impath)
+            imfits[0].data[wfits[0].data == 0.] = np.nan
+            imfits.writeto(doc['image_path'], clobber=True)
+            wfits.close()
+            imfits.close()
 
 
 class TargetWCS(object):
