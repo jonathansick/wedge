ce40b4ae09b4bef88e069d2551d110365c15fa1f
Jonathan Sick <jonathansick@mac.com>
2014-11-05 01:07:23 -0500

    Add table of pixel properties to MultiWedge

--- wedge/multiwedge.py
+++ wedge/multiwedge.py
@@ -8,6 +8,7 @@ cover the entire galaxy disk.
 """
 
 import astropy.io.fits as fits
+from astropy.table import Table
 from astropy.wcs import WCS
 from astropy.coordinates import Distance, Angle, SkyCoord
 from astropy import units as u
@@ -29,8 +30,11 @@ class MultiWedge(object):
         Radial grid should start > 0. Values between 0. and radial_grid[0]
         are assigned to a central elliptical bin.
         """
+        assert radial_grid[0] > 0.
         self._map_pixel_coordinates(coord0, d0, incl0, pa0)
         self._make_segmap(n_wedges, radial_grid)
+        self._make_pixel_table(n_wedges, radial_grid)
+        print self.pixel_table
 
     def _map_pixel_coordinates(self, coord0, d0, incl0, pa0):
         shape = (self.ref_header['NAXIS2'], self.ref_header['NAXIS2'])
@@ -87,6 +91,7 @@ class MultiWedge(object):
                 pa_segmap[inds] = i
                 r_indices = np.digitize(self.image_r[inds], radial_grid,
                                         right=False)
+                segmap[inds] = r_indices
             else:
                 # for non-wrapping wedges
                 inds = np.where((self.image_sky_pa >= pa_grid[i]) &
@@ -96,9 +101,12 @@ class MultiWedge(object):
                 r_indices = np.digitize(self.image_r[inds], radial_grid,
                                         right=False) - 1
                 pa_segmap[inds] = i
+                segmap[inds] = r_indices \
+                    + (radial_grid.shape[0] - 1) * (i - 1) \
+                    + radial_grid.shape[0]
             # Only do PA+radial binning beyond the first bin
             # r_indices[r_indices == (radial_grid.shape[0] - 1)] = np.nan
-            segmap[inds] = r_indices + radial_grid.shape[0] * i
+            # segmap[inds] = r_indices + radial_grid.shape[0] * i
         # Paint central ellipse
         central_inds = np.where(self.image_r <= radial_grid[0])
         print "central_ins", central_inds
@@ -110,6 +118,44 @@ class MultiWedge(object):
         bin_vals = np.unique(segmap.flatten())
         bin_vals.sort()
         print bin_vals
+
+    def _make_pixel_table(self, n_wedges, radial_grid):
+        pa_delta = 360. / float(n_wedges)
+        n_radii = len(radial_grid) - 1
+        n_pixels = n_wedges * n_radii + 1
+
+        # count area of each bin
+        s = np.where(self.segmap >= 0)
+        pix_count = np.bincount(self.segmap[s].flatten())
+
+        A = self._pixel_scale()
+        pix_area = pix_count * A
+
+        # Initialize with central elliptical pixel
+        pix_id = [0]
+        pix_pa = [0.]
+        pix_r_inner = [0.]
+        pix_r_outer = [radial_grid[0]]
+        pix_r_mid = [0.]
+
+        i = 0
+        for j in xrange(n_wedges):
+            for k in xrange(len(radial_grid) - 1):
+                i += 1
+                pix_id.append(i)
+                pix_pa.append(pa_delta * j)
+                pix_r_inner.append(radial_grid[k])
+                pix_r_outer.append(radial_grid[k + 1])
+                pix_r_mid.append(0.5 * (radial_grid[k + 1] + radial_grid[k]))
+
+        print n_pixels, len(pix_area)
+        assert len(pix_id) == n_pixels
+        assert len(pix_area) == n_pixels
+
+        t = Table((pix_id, pix_pa, pix_r_mid, pix_r_inner, pix_r_outer,
+                   pix_area),
+                  names=('ID', 'PA', "R_mid", 'R_inner', 'R_outer', 'area'))
+        self.pixel_table = t
 
     @staticmethod
     def correct_rgc(coord, glx_ctr, glx_PA, glx_incl, glx_dist):
@@ -165,3 +211,11 @@ class MultiWedge(object):
         obj_phi[s] = Angle(2. * np.pi, unit=u.rad) + obj_phi[s]
 
         return obj_dist, obj_phi
+
+    def _pixel_scale(self):
+        if 'CDELT' in self.ref_header:
+            pix_scale = self.ref_header['CDELT'] * 3600.
+        else:
+            pix_scale = np.sqrt(self.ref_header['CD1_1'] ** 2.
+                                + self.ref_header['CD1_2'] ** 2.) * 3600.
+        return pix_scale
