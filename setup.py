8751c035e38d50f314501165cc9b91fd6968d3b7
Jonathan Sick <jsick@lsst.org>
2016-05-30 19:42:18 +0000

    Make setup compatible with astropy_helpers

--- setup.py
+++ setup.py
@@ -16,9 +16,10 @@ else:
 builtins._ASTROPY_SETUP_ = True
 
 import astropy
-from astropy.setup_helpers import (register_commands, adjust_compiler,
-                                   get_debug_option)
-from astropy.version_helpers import get_git_devstr, generate_version_py
+from astropy_helpers.setup_helpers import (register_commands, adjust_compiler,
+                                           get_debug_option)
+from astropy_helpers.version_helpers import generate_version_py
+from astropy_helpers.git_helpers import get_git_devstr
 
 # Set affiliated package-specific settings
 PACKAGENAME = 'wedge'
@@ -48,7 +49,8 @@ cmdclassd = register_commands(PACKAGENAME, VERSION, RELEASE)
 adjust_compiler(PACKAGENAME)
 
 # Freeze build information in version.py
-generate_version_py(PACKAGENAME, VERSION, RELEASE, get_debug_option())
+generate_version_py(PACKAGENAME, VERSION, RELEASE,
+                    get_debug_option(PACKAGENAME))
 
 # Treat everything in scripts except README.rst as a script to be installed
 scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
@@ -71,7 +73,7 @@ except ImportError: # compatibility with Astropy 0.2 - can be removed in cases
                     # where Astropy 0.2 is no longer supported
 
     from setuptools import find_packages
-    from astropy.setup_helpers import filter_packages, update_package_files
+    from astropy_helpers.setup_helpers import filter_packages, update_package_files
 
     package_info = {}
 
